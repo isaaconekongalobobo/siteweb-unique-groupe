dc5daf7d06b66064de3602c9afd49926c2a264c6
isaaconek <onekdev5@gmail.com>
2024-12-06 12:07:47 +0100

    Restructuration des dossier et ficheir du projet

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-const routes = require('./routes');
+const { title } = require('process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,8 +8,6 @@ const PORT = process.env.PORT || 3000;
 // Configuration des vues
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts);
-app.set('layout', 'layouts/main');
 
 // Middleware pour les fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,11 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/', routes);
-
-// Gestion des erreurs 404
-app.use((req, res) => {
-  res.status(404).render('errors/404');
+app.use('/', (req, res) => {
+  res.render ('home', {title: "Acceuil"})
 });
 
 app.listen(PORT, () => {
