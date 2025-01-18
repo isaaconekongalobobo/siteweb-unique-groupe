b1142bdc20d9675968c629ee174bbe98e60f7848
isaaconek <onekdev5@gmail.com>
2025-01-18 15:19:19 +0100

    Categorisation des services et amelioration de premiere section de la page d'acceuil

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': { max: '639px' },
+        'tablet': { min: '640px', max: '1023px' },
+        'desktop': { min: '1024px' },
+      },
+      backgroundColor: {
+        'main-blue': '#1C94C4'
+      }
+    },
   },
   plugins: [],
 }
